a99562d18ed7c520a84cc8d53bc22dd0248784f8
y1笑而过 <15335747148@163.com>
2018-07-19 11:10:44 +0800

    使用组件样式
    使用组件样式

--- 组件样式/使用组件样式/clab/src/app/app.module.ts
+++ 组件样式/使用组件样式/clab/src/app/app.module.ts
@@ -3,14 +3,10 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { AstronautComponent } from './astronaut/astronaut.component';
-import { MissionControlComponent } from './mission-control/mission-control.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AstronautComponent,
-    MissionControlComponent,
   ],
   imports: [
     BrowserModule,
